bc595724c6d6b0cebfdb05de173c7f493cee168a
Ann Wanjeri <wannjeri2@gmail.com>
2022-04-20 21:51:41 +0300

    components and services files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchPageComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
